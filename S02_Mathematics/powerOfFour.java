3fca1b3882e306f219b1599aeb0ed41dfd6de92f
Aditya Raj <it.adityasinha2701@gmail.com>
2025-08-05 12:31:58 +0530

    Add files via upload

---
+++ S02_Mathematics/powerOfFour.java
@@ -0,0 +1,15 @@
+public class powerOfFour {
+     int isPowerOfFour(long n) {
+        // Your code here
+         if(n==0)
+            return 0;
+            while(n!=1)
+            {
+                if(n%4!=0)
+                return 0;
+                n=n/4;
+            }
+            return 1;
+        }
+
+}
