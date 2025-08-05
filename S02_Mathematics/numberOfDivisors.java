fae83d9be4ff782ea063d08926ea4b6ff8d345f3
Aditya Raj <it.adityasinha2701@gmail.com>
2025-08-05 12:30:27 +0530

    Add files via upload

---
+++ S02_Mathematics/numberOfDivisors.java
@@ -0,0 +1,16 @@
+//Given an integer n, find the number of divisors of n that are divisible by 3 
+
+public class numberOfDivisors {
+     static int countDivisors(int n) {
+        // code here
+        int count = 0; 
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                if (i % 3 == 0) {
+                    count++; 
+                }
+            }
+        }
+        return count; 
+    }
+}
