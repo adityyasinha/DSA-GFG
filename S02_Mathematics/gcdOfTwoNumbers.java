571bd883ee8c33260efac2c14f622e015a80d3c5
Aditya Raj <it.adityasinha2701@gmail.com>
2025-08-05 12:25:40 +0530

    Add files via upload

---
+++ S02_Mathematics/gcdOfTwoNumbers.java
@@ -0,0 +1,16 @@
+/* Given two positive integers a and b, find GCD of a and b.
+Note: Don't use the inbuilt gcd function
+*/
+
+public class gcdOfTwoNumbers {
+    public static int gcd(int a, int b) {
+        // code here
+       while ( a != b)
+       if(a > b){
+       a = a-b;
+       } else {
+           b = b-a;
+       }
+       return a;
+    }
+}
