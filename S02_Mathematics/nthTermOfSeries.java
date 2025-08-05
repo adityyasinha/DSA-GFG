b92e572daab299628232b228d399eb18c4c53083
Aditya Raj <it.adityasinha2701@gmail.com>
2025-08-05 12:30:01 +0530

    Add files via upload

---
+++ S02_Mathematics/nthTermOfSeries.java
@@ -0,0 +1,10 @@
+/*
+Given a series 9, 33, 73, 129...  Find the n-th term of the series.
+*/
+
+public class nthTermOfSeries {
+    static long nthOfSeries(long n) {
+        // code here
+        return 8 * n * n + 1;
+    }
+}
