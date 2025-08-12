e621d80b7008095067a47fa89357813de7796e73
Aditya Raj <it.adityasinha2701@gmail.com>
2025-08-12 22:35:54 +0530

    Add files via upload

---
+++ S02_Mathematics/sumOfNumber.java
@@ -0,0 +1,8 @@
+// Given an integer n, your task is to compute the sum of all natural numbers from 1 to n (inclusive). If n is 0, the sum should be 0.
+
+public class sumOfNumber {
+     public static int findSum(int n) {
+        // code here
+        return (n* (n + 1)/2);
+    }   
+}
