1cefafa328800ebade204790ef61ab047a83027b
Aditya Raj <it.adityasinha2701@gmail.com>
2025-08-15 19:59:28 +0530

    Add files via upload

---
+++ S02_Mathematics/sumOfTwoNumbers.java
@@ -0,0 +1,10 @@
+/*
+Given two numbers a and b. Your task is to return the sum of a and b.
+ */
+public class sumOfTwoNumbers {
+     static int addition(int a, int b) {
+        // code here
+        int sum;
+        return sum = a + b;
+    }
+}
