72a907e6fb7fd5cde708f49afaa4e8cee4bb04ed
Aditya Raj <it.adityasinha2701@gmail.com>
2025-08-17 22:22:26 +0530

    Add files via upload

---
+++ S03_Bit_Magic/CheckBit.java
@@ -0,0 +1,16 @@
+/*
+Given two positive integer n and  k, check if the kth index bit of n is set or not.
+ Note: A bit is called set if it is 1. 
+*/
+
+
+public class CheckBit {
+    static boolean checkKthBit(int n, int k) {
+        // code here
+        if((n & (1 << k)) != 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
