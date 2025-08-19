798125b5075a60f7e24356c1ee9809a9e1e8b66f
Aditya Raj <it.adityasinha2701@gmail.com>
2025-08-19 22:55:11 +0530

    Add files via upload

---
+++ S07_Sorting/SortArrays.java
@@ -0,0 +1,8 @@
+//Given an unsorted array arr[] of numbers, sort the array in ascending order
+
+class Solution {
+    void sortArr(int[] arr) {
+        // code here
+        Arrays.sort(arr);
+    }
+}
