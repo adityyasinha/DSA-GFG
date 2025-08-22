638fdab9803c9247da72792ecdf5df58d5b9302b
Aditya Raj <it.adityasinha2701@gmail.com>
2025-08-22 22:38:30 +0530

    Add files via upload

---
+++ S03_Bit_Magic/MissingInArrays.java
@@ -0,0 +1,14 @@
+public class MissingInArrays {
+     int missingNum(int arr[]) {
+        // code here
+        int n = arr.length + 1; // Since one number is missing
+        long expectedSum = (long) n * (n + 1) / 2;
+
+        long actualSum = 0;
+        for (int num : arr) {
+            actualSum += num;
+        }
+
+        return (int) (expectedSum - actualSum);
+    }
+}
