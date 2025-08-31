5c44a02c28baa99662e9bde19f873dc7b1376a88
Aditya Raj <it.adityasinha2701@gmail.com>
2025-08-31 21:22:34 +0530

    Add files via upload

---
+++ S05_Array/ArrayInsertAtEnd.java
@@ -0,0 +1,12 @@
+/*
+Given an array arr that is not completely filled and a value a, you have to insert the value at the end of the array.
+*/
+
+import java.util.ArrayList;
+
+public class ArrayInsertAtEnd {
+     public void insertAtEnd(ArrayList<Integer> arr, int val) {
+        // code here
+        arr.add(val);
+    }
+}
