feaa766d0cc950e1beb05ac76f025a1712c6de64
Aditya Raj <it.adityasinha2701@gmail.com>
2025-09-03 14:21:29 +0530

    Add files via upload

---
+++ S06_Searching/RemoveDuplicateFromSorted.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class RemoveDuplicateFromSorted {
+     // Function to remove duplicates from the given array.
+    ArrayList<Integer> removeDuplicates(int[] arr) {
+        ArrayList<Integer> result = new ArrayList<>();
+
+        if (arr.length == 0) return result;
+
+        result.add(arr[0]); // First element is always unique
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[i - 1]) {
+                result.add(arr[i]);
+            }
+        }
+
+        return result;
+    }
+}
