43d2b1681bf20f128601d049df6d89796aa3f5b0
Aditya Raj <it.adityasinha2701@gmail.com>
2025-09-03 14:09:38 +0530

    Add files via upload

---
+++ S05_Array/MoveAllZerosToEnd.java
@@ -0,0 +1,18 @@
+/*
+You are given an array arr[] of non-negative integers. Your task is to move all the zeros in the array to the right end while maintaining the relative order of the non-zero elements. The operation must be performed in place, meaning you should not use extra space for another array.
+*/
+public class MoveAllZerosToEnd {
+    void pushZerosToEnd(int[] arr) {
+        // code here
+        int count = 0;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i]!=0)
+            {
+                int temp = arr[i];
+                arr[i] = arr[count];
+                arr[count] = temp;
+                count++;
+            }
+        }
+    }
+}
