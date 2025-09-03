118b4e53012ebbbd2b028242486b70d676e5f26e
Aditya Raj <it.adityasinha2701@gmail.com>
2025-09-03 14:04:37 +0530

    Add files via upload

---
+++ S05_Array/Search.java
@@ -0,0 +1,12 @@
+//Given an array, arr of n integers, and an integer element x, find whether element x is present in the array. Return the index of the first occurrence of x in the array, or -1 if it doesn't exist.
+
+public class Search {
+    static int search(int arr[], int x) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == x) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
