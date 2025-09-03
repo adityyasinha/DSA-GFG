a3d70db52109763d193b90e1a96326c96db326f2
Aditya Raj <it.adityasinha2701@gmail.com>
2025-09-03 13:59:04 +0530

    Add files via upload

---
+++ S05_Array/Solution.java
@@ -0,0 +1,6 @@
+public class Solution {
+    public String maxWater(int[] arr) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'maxWater'");
+    }
+}
