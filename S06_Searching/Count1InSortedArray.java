ae457ae788a4576eca4e64f032d2e55bca5fae35
Aditya Raj <it.adityasinha2701@gmail.com>
2025-09-21 19:14:44 +0530

    Add files via upload

---
+++ S06_Searching/Count1InSortedArray.java
@@ -0,0 +1,19 @@
+public class Count1InSortedArray {
+     public int countOnes(int[] arr) {
+        int low = 0, high = arr.length - 1;
+        int lastOneIndex = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == 1) {
+                lastOneIndex = mid;
+                low = mid + 1; // Search on the right side
+            } else {
+                high = mid - 1; // Search on the left side
+            }
+        }
+
+        return lastOneIndex + 1;
+    }
+}
