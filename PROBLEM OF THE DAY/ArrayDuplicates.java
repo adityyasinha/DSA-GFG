7cb3f39185f898a5b2b30d2aab603255edac87d2
Aditya Raj <it.adityasinha2701@gmail.com>
2025-12-09 21:37:37 +0530

    Add method to find duplicates in an array
    Implement findDuplicates method to identify duplicate integers in an array.

---
+++ PROBLEM OF THE DAY/ArrayDuplicates.java
@@ -0,0 +1,14 @@
+class Solution {
+    public ArrayList<Integer> findDuplicates(int[] arr) {
+        // code here
+         ArrayList<Integer> ans = new ArrayList<>();
+        
+        for (int i = 0; i < arr.length; i++) {
+            int idx = Math.abs(arr[i]) - 1;
+            if (arr[idx] < 0) ans.add(idx + 1);
+            else arr[idx] = -arr[idx];
+        }
+        
+        return ans;
+    }
+}
